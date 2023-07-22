a7b8a2c002244091cc1debd8f37d69c07538c5c1
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-22 14:36:10 -0500

    added some stuff in TODO, moved math functions to sever.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,19 +4,21 @@ function onReady() {
     console.log("HERE HERE!");
 
     // add, subtract, multiply, divide... so far
-    $('#add').on('click', addition);
-    $('#subtract').on('click', subtraction);
-    $('#multiply').on('click', multiplication);
-    $('#divide').on('click', division);
+    //$('#add').on('click', addition);
+    //$('#subtract').on('click', subtraction);
+    //$('#multiply').on('click', multiplication);
+    //$('#divide').on('click', division);
 
     $('#equals').on('click', equals);
 
     $('#clear').on('click', clear);
-
+    
 }
 
+let numbers =[]
+
 // functions addNumbers, subtractNumbers, multiplyNumbers, divideNumbers... so far
-function addition(){
+/*function addition(){
     console.log('in addition');
 }
 
@@ -32,12 +34,15 @@ function division(){
     console.log('in division');
 }
 
-function clear() {
-    console.log('in clear');
-    
-}
 
+*/
 function equals() {
     console.log('in equals');
+
+}
+
+function clear() {
+    console.log('in clear');
+    //$().remove();
 
 }
--- server/server.js
+++ server/server.js
@@ -9,10 +9,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+// let's see if the two number inputs requested constitute an array
+let numbers2BeCalculated = []
+
+// functions addNumbers, subtractNumbers, multiplyNumbers, divideNumbers... so far
+function addition(){
+    console.log('in addition');
+}
+
+function subtraction(){
+    console.log('in subtraction');
+}
+
+function multiplication(){
+    console.log('in multiplication');
+}
+
+function division(){
+    console.log('in division');
+}
+
+
+
+
+
+app.get('/numbers', (req, res) => {
+    console.log("Arrived at /numbers", numbers2BeCalculated);
+
+
+
+
+    // Server responds with the playerGuesses
+    res.send(numbers2BeCalculated);
+
+})
+
 
 
 
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
-  })
-  
+})
