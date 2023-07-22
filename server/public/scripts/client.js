70fb80cd9e9dcd8ec7969fbd99b8d0bb9da825af
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-21 20:19:50 -0500

    listeners/functions for operands console logging

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,29 @@
 $(document).ready(onReady);
 
-function onReady(){
-console.log("HERE HERE!");
+function onReady() {
+    console.log("HERE HERE!");
+
+    // add, subtract, multiply, divide... so far
+    $('#add').on('click', addNumbers);
+    $('#subtract').on('click', subtractNumbers);
+    $('#multiply').on('click', multiplyNumbers);
+    $('#divide').on('click', divideNumbers);
 
 }
 
+// functions addNumbers, subtractNumbers, multiplyNumbers, divideNumbers... so far
+function addNumbers(){
+    console.log('in addNumbers');
+}
+
+function subtractNumbers(){
+    console.log('in subtractNumbers');
+}
+
+function multiplyNumbers(){
+    console.log('in multiplyNumbers');
+}
+
+function divideNumbers(){
+    console.log('in subtractNumbers');
+}
