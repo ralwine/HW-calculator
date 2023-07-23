2d5309191eb1f6c24fe659772c6e1810bad53709
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-23 15:50:58 -0500

    started the scripts over and no dice

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,48 +1,124 @@
 $(document).ready(onReady);
 
+let calculation;
+// array of operands to send over to convert into commands, perhaps?
+let operands = ['+', '-', '*', '/']
+
+let sum
+
 function onReady() {
     console.log("HERE HERE!");
 
     // add, subtract, multiply, divide... so far
-    //$('#add').on('click', addition);
-    //$('#subtract').on('click', subtraction);
-    //$('#multiply').on('click', multiplication);
-    //$('#divide').on('click', division);
+    /*
+    $('#add').on('click', operands);
+    $('#subtract').on('click', operands);
+    $('#multiply').on('click', operands);
+    $('#divide').on('click', operands);
+    */
 
-    $('#equals').on('click', equals);
+    $('#equals').on('click', submitCalculation);
+    getCalculation();
+    //$('.input').on('click', clear);
 
-    $('#clear').on('click', clear);
-    
 }
 
-let numbers =[]
+/*
 
-// functions addNumbers, subtractNumbers, multiplyNumbers, divideNumbers... so far
-/*function addition(){
+function addition() {
     console.log('in addition');
 }
 
-function subtraction(){
+function subtraction() {
     console.log('in subtraction');
 }
 
-function multiplication(){
+function multiplication() {
     console.log('in multiplication');
 }
 
-function division(){
+function division() {
     console.log('in division');
 }
 
-
-*/
 function equals() {
     console.log('in equals');
+
+}
+*/
+let submitCalculation = event => {
+    event.preventDefault();
+    console.log('in submitCalculation');
+    const inputNumbers = {
+        numberOne: $('#firstNumberInput').val(),
+        numberTwo: $('#secondNumberInput').val(),
+        operands: $('.operands').on('click')
+    }
+
+
+    // Ajax will communicate and send dat to the server
+    $.ajax({
+        method: "POST", // type of request
+        url: "/submitcalc", //route that will be used
+        data: inputNumbers// Needs to be an object
+    }).then((response) => {
+        console.log("POST was successful", response) //Expect 201
+        //getNumbers();
+        //render();
+    }).catch((error) => {
+        console.log("Error with POST request", error);
+        alert("Error with POST");
+
+
+        getCalculation();
+        render();
+    })
+    //$('#firstNumberInput').val("");
+    //$('#secondNumberInput').val("");
+
+    // Take some data and send to server
+}
+
+let getCalculation = () => {
+    // Use Ajax to retrieve (GET) quotes from server
+    // Server endpooint: /quotes
+    console.log('inside result');
+    //   AJAX
+    $.ajax({
+        // Tells Ajax how to communicate with server,
+        // also tells Ajax where to go on the server
+        method: 'GET',
+        url: '/returncalc'
+    }).then((response) => {
+        console.log(response);
+        sum = response //Adds our numbers to the DOM (as long as the server is still running)
+        render();
+
+    }).catch((error) => {
+        alert("Request failed")
+        console.log("Request failed", error)
+    })
+}
+
+let render = () => {
+    $('#output').empty();
+    // Looping over and appending to DOM
+    for (let calculation of sum) {
+        console.log(calculation);
+        $('#output').append(`
+            <li>
+            ${calculation}
+            </li>
+      `)
+
+    }
 
 }
 
 function clear() {
     console.log('in clear');
-    //$().remove();
+    $().val().remove();
 
 }
+
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = 5000;
+const port = 5000;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,44 +9,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// let's see if the two number inputs requested constitute an array
-let numbers2BeCalculated = []
+// let's see if the two number inputs requested constitute an array or not
+let calculations = [
+    
+]
+
+// Create GET route
+    // Has a path: /returncalc
+    // Has anonymous function: has req and res
+    app.get('/returncalc', (req, res) => {
+        console.log("Arrived at /quotes", calculations)
+    
+        // Server is responding with the calculations
+        res.send(calculations)
+        // res.sendStatus(200)
+    })
+    
+
+    
+    app.post('/submitcalc',(req, res) => {
+        console.log("Body for submitcalculation:", req.body);
+    
+        let newCalculation = req.body
+        calculations.push(newCalculation)
+    
+        console.log("currentCalculations", calculations)
+        res.sendStatus(201)
+    })
+
+
 
 // functions addNumbers, subtractNumbers, multiplyNumbers, divideNumbers... so far
-function addition(){
-    console.log('in addition');
-}
-
-function subtraction(){
-    console.log('in subtraction');
-}
-
-function multiplication(){
-    console.log('in multiplication');
-}
-
-function division(){
-    console.log('in division');
-}
-
-
-
-
-
-app.get('/numbers', (req, res) => {
-    console.log("Arrived at /numbers", numbers2BeCalculated);
-
-
-
-
-    // Server responds with the playerGuesses
-    res.send(numbers2BeCalculated);
-
-})
-
-
-
-
-app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+app.listen(port, () => {
+    console.log('listening on port', port)
 })
